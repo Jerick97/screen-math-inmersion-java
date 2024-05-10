153ee9c1cfb0bbcf3bd96b2e1e6bde0eae86a8de
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-05-09 19:25:58 -0500

    refactor(backend): Refactorización de la clase Main y creación del paquete modelo

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,11 @@
-import java.text.DecimalFormat;
+import modelo.Pelicula;
+import modelo.Serie;
+
 import java.util.Scanner;
-import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Bienvenidos a la inmersión en Java!");
-        //  System.out.println("Película Matrix");
-
-        // Declaración de Variables
-        int fechaDeLanzamiento = 1999;
-        double evaluacion = 4.5;
-        boolean incluidoEnElPlanBasico = true;
-        String nombre = "Matrix";
-        String sinopsis = """
-                La mejor película del fin del milenio
-                """;
-        double mediaEvaluacionUsuario = 0.0;
-        Scanner teclado = new Scanner(System.in); // Leer entrada del usuario
-        teclado.useLocale(Locale.ENGLISH); // Establecer el separador decimal como el punto
-        DecimalFormat df = new DecimalFormat("#.00"); //Formatear a 2 decimales
-
-        // Detalles de la Película
-        System.out.println("Detalles de la Película:");
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Fecha de Lanzamiento: " + fechaDeLanzamiento);
-        System.out.println("Evaluación: " + evaluacion);
-        System.out.println("Incluida en el Plan Básico: " + (incluidoEnElPlanBasico ? "Sí" : "No"));
-
-        double mediaEvaluacion = (4.5 + 4.8 + 3 ) / 3;
-        System.out.println("Media de la evaluación de Matrix: " + df.format(mediaEvaluacion));
-        
-        // Condicionales
-
-        if(fechaDeLanzamiento >= 2024){
-            System.out.println("Película popular en el momento");
-        } else {
-            System.out.println("Película retro que vale la pena ver");
-        }
-
-        // Bucles
-        // For
-        /*
-            for (int i = 0; i < 3; i++) {
-
-                System.out.println("Ingresa la evaluación que le darías a " + nombre + ": ");
-
-                double notaMatrix = obtenerEvaluacion(teclado);
-                mediaEvaluacionUsuario += notaMatrix;
-            }
-        */
-
-        // While
-
-        char continuar = 's'; // Variable para controlar si el usuario desea continuar
-        int contadorEvaluaciones = 0; // Contador para el número de evaluaciones ingresadas
-
-        while (continuar == 's' || continuar == 'S') {
-            System.out.println("Ingresa la evaluación que le darías a " + nombre + ": ");
-
-            double notaMatrix = obtenerEvaluacion(teclado);
-            mediaEvaluacionUsuario += notaMatrix;
-            contadorEvaluaciones++; //incrementa +1
-
-            System.out.println("¿Deseas ingresar otra evaluación? (s/n): ");
-            continuar = teclado.next().charAt(0); // Leer la respuesta del usuario
-        }
-
-        teclado.close(); // Cerrar el Scanner después de usarlo
-        System.out.println("La media de la película " + nombre + " es "+ df.format(mediaEvaluacionUsuario / contadorEvaluaciones));
-    }
-
-    public static double obtenerEvaluacion(Scanner teclado) {
-        while (!teclado.hasNextDouble()) { // Manejar caracteres no numéricos
-            System.out.println("Entrada inválida. Por favor ingresa un número válido.");
-            teclado.next(); // Descartar la entrada no válida
-        }
-        return teclado.nextDouble();
+        Principal principal = new Principal();
+        principal.muestraElMenu();
     }
 }
---
+++ src/Principal.java
@@ -0,0 +1,80 @@
+import modelo.Pelicula;
+import modelo.Serie;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Pelicula peliculaUsuario = new Pelicula();
+    private Serie serieUsuario = new Serie();
+    public void muestraElMenu(){
+        int opcion = 0;
+        Scanner teclado = new Scanner(System.in);
+        while(opcion != 9){
+          String menu = """
+                 Bienvenido(a) a Screenmatch
+                 
+                 1) Registrar nueva película
+                 2) Registrar nueva serie
+                 9) Salir
+                 
+                  """;
+            System.out.println(menu);
+            opcion = validarEntero(teclado);
+            teclado.nextLine();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingrese el nombre de la película");
+                    String nombre = teclado.nextLine();
+                    System.out.println("Ingrese el año de lanzamiento de la película");
+                    int fechaDeLanzamiento = validarEntero(teclado);
+                    teclado.nextLine();
+                    System.out.println("Ingrese la duración en minutos de la película");
+                    int duracionEnMinutos = validarEntero(teclado);
+                    teclado.nextLine();
+
+                    peliculaUsuario.setNombre(nombre);
+                    peliculaUsuario.setTiempoDeDuracionEnMinutos(duracionEnMinutos);
+                    peliculaUsuario.setFechaDeLanzamiento(fechaDeLanzamiento);
+                    peliculaUsuario.muestraFichaTecnica();
+                    break;
+                case 2:
+                    System.out.println("Ingrese el nombre de la serie");
+                    String nombreSerie = teclado.nextLine();
+                    System.out.println("Ingrese el año de lanzamiento de la serie");
+                    int fechaDeLanzamientoSerie = validarEntero(teclado);
+                    teclado.nextLine();
+                    System.out.println("Por favor ingresa el numero de temporadas para esta serie");
+                    int temporadas = validarEntero(teclado);
+                    teclado.nextLine();
+                    System.out.println("Por favor ingresa la cantidad de episodios por cada temporada");
+                    int episodiosPorTemporada = validarEntero(teclado);
+                    teclado.nextLine();
+                    System.out.println("Por favor ingresa la duración en minutos de cada episodio");
+                    int minutosPorEpisodio = validarEntero(teclado);
+                    teclado.nextLine();
+
+                    serieUsuario.setNombre(nombreSerie);
+                    serieUsuario.setFechaDeLanzamiento(fechaDeLanzamientoSerie);
+                    serieUsuario.setTemporadas(temporadas);
+                    serieUsuario.setEpisodiosPorTemporadas(episodiosPorTemporada);
+                    serieUsuario.setDuracionEnMinutosPorEpisodios(minutosPorEpisodio);
+                    serieUsuario.muestraFichaTecnica();
+                    break;
+                case 9:
+                    System.out.println("Saliendo del programa");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }
+    }
+
+    public static int validarEntero(Scanner teclado) {
+        while (!teclado.hasNextInt()) { // Manejar caracteres no numéricos
+            System.out.println("Entrada inválida. Por favor ingresa un número entero válido.");
+            teclado.next(); // Descartar la entrada no válida
+        }
+        return teclado.nextInt();
+    }
+}
